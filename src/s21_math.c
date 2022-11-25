dc13e786e98bf399ec81ece00de0d328210f18ee
Amur312 <dzarasovamur@gmail.com>
2022-11-25 18:03:18 +0300

    25.11.2022

--- src/s21_math.c
+++ src/s21_math.c
@@ -3,53 +3,66 @@
 int main() {
   double i;
   scanf("%lf", &i);
-  sqrt(i);
+  s21_exp(i);
 }
 
-int abc(int x) {
+// int abc(int x) {
+//   if (x < 0)
+//     x *= -1;
+//   return x;
+// }
+// long double acos(double x) {}
+// long double asin(double x) {}
+// long double atan(double x) {}
+// long double ceil(double x) {}
+// long double cos(double x) {}
+// При переполнении функция возвращает S21_INF (бесконечность)
+// p - храниться значение последнего вычисленного слагаемого
+long double s21_exp(double x) {
+  long double exp_f = 1, n = 1, p = 1;
+  while (s21_fabs(p) > s21_EPS) {
+    p *= x / n;
+    exp_f += p;
+    n++;
+    if (exp_f > MAX_DOUBLE) {
+      exp_f = s21_INF;
+      break;
+    }
+  }
+  return exp_f;
+}
+
+long double s21_fabs(double x) {
   if (x < 0)
     x *= -1;
   return x;
 }
-long double acos(double x) {}
-long double asin(double x) {}
-long double atan(double x) {}
-long double ceil(double x) {}
-long double cos(double x) {}
+// long double floor(double x) {}
+// long double fmod(double x, double y) {}
+// long double log(double x) {}
 
-long double exp(double x) { double e; }
+// long double pow(double base, double exp) {
+//   long double rez;
+//   if (exp == 0) {
+//     rez = 1;
+//     return rez;
+//   } else if (exp < 0) {
 
-long double fabs(double x) {
-  if (x < 0)
-    x *= -1;
-  return x;
-}
-long double floor(double x) {}
-long double fmod(double x, double y) {}
-long double log(double x) {}
+//   } else if (exp > 0) {
+//   }
+// }
 
-long double pow(double base, double exp) {
-  long double rez;
-  if (exp == 0) {
-    rez = 1;
-    return rez;
-  } else if (exp < 0) {
+// long double sin(double x) {}
 
-  } else if (exp > 0) {
-  }
-}
+// long double sqrt(double x) {
+//   double esp = 10e-8;
+//   long double s = x;
 
-long double sin(double x) {}
+//   while ((s - x / s) > esp) {
+//     s = (s + x / s) / 2;
+//   }
 
-long double sqrt(double x) {
-  double esp = 10e-8;
-  long double s = x;
+//   return s;
+// }
 
-  while ((s - x / s) > esp) {
-    s = (s + x / s) / 2;
-  }
-
-  return s;
-}
-
-long double tan(double x) {}
+// long double tan(double x) {}
