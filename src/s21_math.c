7e1885cd21b6c4aa0835413d383e3bce4886e4c1
Amur312 <dzarasovamur@gmail.com>
2022-11-26 17:14:09 +0300

    26.11.2022

--- src/s21_math.c
+++ src/s21_math.c
@@ -3,14 +3,14 @@
 int main() {
   double i;
   scanf("%lf", &i);
-  s21_exp(i);
+  s21_log(i);
 }
 
-// int abc(int x) {
-//   if (x < 0)
-//     x *= -1;
-//   return x;
-// }
+int abc(int x) {
+  if (x < 0)
+    x *= -1;
+  return x;
+}
 // long double acos(double x) {}
 // long double asin(double x) {}
 // long double atan(double x) {}
@@ -18,6 +18,7 @@ int main() {
 // long double cos(double x) {}
 // При переполнении функция возвращает S21_INF (бесконечность)
 // p - храниться значение последнего вычисленного слагаемого
+// exp_f- накопления суммы слагаемых
 long double s21_exp(double x) {
   long double exp_f = 1, n = 1, p = 1;
   while (s21_fabs(p) > s21_EPS) {
@@ -39,17 +40,21 @@ long double s21_fabs(double x) {
 }
 // long double floor(double x) {}
 // long double fmod(double x, double y) {}
-// long double log(double x) {}
-
+long double s21_log(double x) {
+ 
+  // Вычисляем экспоненту как сумму ряда Тейлора
+  int long n = 1;
+  double b = 1;
+  double sn = 1;
+  while (sn > s21_EPS) {
+    sn = sn * (x - 1 )/ n;
+    b = b + sn;
+    n = n++;
+  }
+  printf("%lf",b);
+}
 // long double pow(double base, double exp) {
-//   long double rez;
-//   if (exp == 0) {
-//     rez = 1;
-//     return rez;
-//   } else if (exp < 0) {
 
-//   } else if (exp > 0) {
-//   }
 // }
 
 // long double sin(double x) {}
--- src/s21_math.h
+++ src/s21_math.h
@@ -2,13 +2,16 @@
 #define _S21_MATH_H_
 #define PI 3,1415926535
 #define MAX_DOUBLE 1.7976931348623158e308
-#define MIN_DOUBLE 	-2.2250738585072014e308
+
 #define s21_INF 1.0 / 0.0 // бесконечность
-#define NEG_INF -1.0 / 0.0 // - бесконечность
+
 #define s21_NAN 0.0 / 0.0
 #define s21_EPS 1e-17 
 #include <stdio.h>
-// int abc(int i);
+
+
+int abc(int i);
+long double s21_log(double x);
 // long double sqrt(double x);
 long double s21_fabs(double x);
 long double s21_exp(double x);
