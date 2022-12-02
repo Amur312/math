1530d1e7a3b18b3d5ff75c2e316273654002897c
Amur312 <dzarasovamur@gmail.com>
2022-12-02 11:04:54 +0300

    02.12.2022

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,18 +1,32 @@
-#ifndef _S21_MATH_H_
-#define _S21_MATH_H_
-#define PI 3,1415926535
-#define MAX_DOUBLE 1.7976931348623158e308
+// created by pizpotli, tokenpah, lauriege, muquele
+#ifndef SRC_S21_MATH_H_
+#define SRC_S21_MATH_H_
 
-#define s21_INF 1.0 / 0.0 // бесконечность
-
-#define s21_NAN 0.0 / 0.0
-#define s21_EPS 1e-17 
 #include <stdio.h>
 
+#define s21_PI 3.141592653589793238462643
+#define s21_EPS 1e-20
+#define s21_EXP 2.7182818284590452353602874713526624
+#define s21_MAX_DOUBLE 1.7976931348623158e+308
+#define s21_MAX_INT 2147483647
+#define s21_INF 1.0 / 0.0
+#define s21_NAN 0.0 / 0.0
 
-int abc(int i);
-long double s21_log(double x);
-// long double sqrt(double x);
-long double s21_fabs(double x);
+long double s21_tan(double x);
+long double s21_cos(double x);
+long double s21_sin(double x);
+long double s21_ceil(double x);
+long double s21_floor(double x);
+long double s21_atan(double x);
+long double s21_acos(double x);
+long double s21_asin(double x);
+long double s21_fmod(double x, double y);
 long double s21_exp(double x);
-#endif // _S21_MATH_H_
+long double s21_log(double x);
+long double s21_sqrt(double x);
+long double s21_pow(double base, double exp);
+long double s21_fabs(long double x);
+int s21_abs(int x);
+double abs_sravnenie(double a1, double a2);
+
+#endif  // SRC_S21_MATH_H_
